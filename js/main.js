383e37cdfc7513e97cf4d6f2f996b599086e8e27
ToryaToria <torya.toria@yandex.ru>
2024-10-31 13:22:02 +0300

    ДЗ-4 выполнено

--- js/main.js
+++ js/main.js
@@ -1,67 +1,72 @@
 const DESCRIPTIONS = [
-  "Курорт",
-  "указатель пути",
-  "Красивый пляж",
-  "Сиськи и фотик",
-  "типа супчик",
-  "машина Бетмена",
-  "урожай клубники не удался",
-  "А ты налей и отойди",
-  "Спасение с необитаемого острова",
-  "Кто первый того и тапки",
-  "вход в лабиринт",
-  "красивая белая машинка",
-  "тошниловка",
-  "котамбургер",
-  "в детстве мало пароли",
-  "пролетая над Парижем",
-  "хор мальчиков-зайчиков",
-  "я в машинах не разбираюст, но эта мне нравится",
-  "хочу такие тапочки с подсветкой",
-  "пальмы",
-  "а нормальной еды нет?",
-  "ухожу в закат",
-  "обитатель моря",
-  "слушают руками",
-  "Вам помочь?"
+  'Курорт',
+  'указатель пути',
+  'Красивый пляж',
+  'Сиськи и фотик',
+  'типа супчик',
+  'машина Бетмена',
+  'урожай клубники не удался',
+  'А ты налей и отойди',
+  'Спасение с необитаемого острова',
+  'Кто первый того и тапки',
+  'вход в лабиринт',
+  'красивая белая машинка',
+  'тошниловка',
+  'котамбургер',
+  'в детстве мало пароли',
+  'пролетая над Парижем',
+  'хор мальчиков-зайчиков',
+  'я в машинах не разбираюст, но эта мне нравится',
+  'хочу такие тапочки с подсветкой',
+  'пальмы',
+  'а нормальной еды нет?',
+  'ухожу в закат',
+  'обитатель моря',
+  'слушают руками',
+  'Вам помочь?'
 ];
 
 const COMMENTS_ARR = [
-  "Всё отлично!",
-  "В целом всё неплохо. Но не всё.",
-  "Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.",
-  "Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.",
-  "Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.",
-  "Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!"
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
 const NAMES = [
-  "Услада",
-  "Полина-Полина",
-  "Голуба",
-  "Апрель",
-  "Вишня",
-  "Принцесса Даниэлла",
-  "Заря-Заряница",
-  "Луна",
-  "Луналика",
-  "Ярослав",
-  "Лютобор",
-  "Каспер",
-  "Ненаглядный",
-  "Дельфин"
+  'Услада',
+  'Полина-Полина',
+  'Голуба',
+  'Апрель',
+  'Вишня',
+  'Принцесса Даниэлла',
+  'Заря-Заряница',
+  'Луна',
+  'Луналика',
+  'Ярослав',
+  'Лютобор',
+  'Каспер',
+  'Ненаглядный',
+  'Дельфин'
 ];
 
-
 const PHOTOS_COUNT_MAX = 25;
-const likes = {
-MIN: 15,
-MAX: 200
-}
+const LIKES = {
+  MIN: 15,
+  MAX: 200
+};
 
-const COMMENT_MAX = 30;
-const AVA_MAX = 6;
+const COMMENT = {
+  MIN: 1,
+  MAX: 30
+};
 
+const AVA = {
+  MIN: 1,
+  MAX: 6
+};
 
 // функция генерации случайного числа из заданного диапазона
 const getRandomInteger = (a, b) => {
@@ -71,55 +76,46 @@ const getRandomInteger = (a, b) => {
   return Math.floor(result);
 };
 
-
 // получение случайного элемент из массива
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 
-// создание объекта
-const createPhoto = () => {
+// создание сообщения в комментарии
+const createMessage = () => Array.from(
+  { length: getRandomInteger(1, 2)},
+  () => getRandomArrayElement(COMMENTS_ARR)).join(' ');
 
-  let i = 1;
+// ренератор id для комментариев, сквозная нумерация начиная с 1
+
+const createIdGenerator = () => {
+  let namId = 0;
+  // функция с замыканием
   return () => {
-
-    const numComments = getRandomInteger(0, COMMENT_MAX); // кол-во комментариев
-    const numLikes = getRandomInteger(likes.MIN, likes.MAX); // кол-во лайков
-
-    const photo = {}; // создаю объект 
-
-    photo.id = id; // от 1 до 25
-    photo.url = `photos/${id}.jpg`;
-    photo.descriptor = DESCRIPTIONS[id];
-    photo.likes = numLikes;
-    photo.comments = Array.from({ length: numComments }, createComment());
-
-    id++;
-
-    return photo;
-  }
+    namId = namId + 1;
+    return namId;
+  };
 };
 
+const generateRandomId = createIdGenerator();
 
 // создание комментария
-const createComment = () => {
-  return () => {
+const createComment = () => ({
+  id: generateRandomId(),
+  avatar: `img/avatar-${getRandomInteger(AVA.MIN, AVA.MAX)}.svg`,
+  message: createMessage(),
+  name: getRandomArrayElement(NAMES),
+});
 
-    const numCommentMessage = getRandomInteger(1, 2); // из скольки предложений будет формироваться комментарий
-    const commentId = getRandomInteger(1, 1000);
-    const idAva = getRandomInteger(1, 6);
 
-    const comment = {};
-
-    comment.id = commentId; // не должно повторяться!!!!
-    comment.avatar = `img/avatar-${idAva}.svg`;
-    comment.message = 'надо подумать';
-    comment.name = getRandomArrayElement(NAMES);  // случайный элемент из массива NAMES
-
-    return comment;
-  }
-}
-
+// создание объекта
+const createPhoto = (index) => ({
+  id: index,
+  url: `photos/${index}.jpg`,
+  description: DESCRIPTIONS[index],
+  likes: getRandomInteger(LIKES.MIN, LIKES.MAX),
+  comments: Array.from({ length: getRandomInteger(COMMENT.MIN, COMMENT.MAX) }, createComment)
+});
 
 // создание массива объектов
-const PhotosArray = Array.from({ length: PHOTOS_COUNT_MAX }, createPhoto);
+const PhotosArray = Array.from({ length: PHOTOS_COUNT_MAX}, (_, index) => createPhoto(index + 1));
 console.log(PhotosArray);
